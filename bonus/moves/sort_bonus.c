ef72a62e5aa49ffdbcfe4ac8c28c5cce46b655dc
Anas Elkadiri <aelkadir@e2r2p14.1337.ma>
2025-02-03 19:29:51 +0100

    mziana

--- bonus/moves/sort_bonus.c
+++ bonus/moves/sort_bonus.c
@@ -16,6 +16,15 @@ void	excute_move(t_list **stack_a, t_list **stack_b, char *str)
 {
 	if (!ft_strncmp(str, "sa", 2))
 		swap(*stack_a);
+	else if (!ft_strncmp(str, "rra", 3))
+		rev_rotate(stack_a);
+	else if (!ft_strncmp(str, "rrb", 3))
+		rev_rotate(stack_b);
+	else if (!ft_strncmp(str, "rrr", 3))
+	{
+		rev_rotate(stack_a);
+		rev_rotate(stack_b);
+	}
 	else if (!ft_strncmp(str, "sb", 2))
 		swap(*stack_b);
 	else if (!ft_strncmp(str, "ss", 2))
@@ -36,13 +45,4 @@ void	excute_move(t_list **stack_a, t_list **stack_b, char *str)
 		push(stack_a, stack_b);
 	else if (!ft_strncmp(str, "pb", 2))
 		push(stack_b, stack_a);
-	else if (!ft_strncmp(str, "rra", 3))
-		rev_rotate(stack_a);
-	else if (!ft_strncmp(str, "rrb", 3))
-		rev_rotate(stack_b);
-	else if (!ft_strncmp(str, "rrr", 3))
-	{
-		rev_rotate(stack_a);
-		rev_rotate(stack_b);
-	}
 }
